3f8eeab26203f98f131350bcb76b8eefc97c5aa6
kkkgeroge065 <125880010+kkkgeroge065@users.noreply.github.com>
2023-07-26 11:05:34 +0300

    Update weight_scale.py

--- weight_scale.py
+++ weight_scale.py
@@ -2,6 +2,7 @@ print("Добро пожаловать в программу 'Весы'!")
 weight_1 = float(input("Введите вес первого объекта: "))
 weight_2 = float(input("Введите вес второго объекта: "))
 
+
 if weight_1 > weight_2:
     print("Первый объект тяжелее второго.")
 elif weight_1 < weight_2:
